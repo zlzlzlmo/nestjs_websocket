d549223ccd409f15f4be2a0f6af87a0185a51db7
신승훈 <>
2022-06-01 16:50:06 +0900

    message 등록, 및 join 추가

--- src/message/message.gateway.ts
+++ src/message/message.gateway.ts
@@ -1,34 +1,67 @@
-import { WebSocketGateway, SubscribeMessage, MessageBody } from '@nestjs/websockets';
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  WebSocketServer,
+  ConnectedSocket,
+} from '@nestjs/websockets';
 import { MessageService } from './message.service';
 import { CreateMessageDto } from './dto/create-message.dto';
-import { UpdateMessageDto } from './dto/update-message.dto';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
 export class MessageGateway {
+  @WebSocketServer()
+  server: Server;
+
   constructor(private readonly messageService: MessageService) {}
 
   @SubscribeMessage('createMessage')
-  create(@MessageBody() createMessageDto: CreateMessageDto) {
-    return this.messageService.create(createMessageDto);
+  // 클라이언트가 요청할때 create 작동
+  async create(@MessageBody() createMessageDto: CreateMessageDto) {
+    const message = await this.messageService.create(createMessageDto);
+    this.server.emit('meesage', message);
+    return message;
   }
 
+  // 방에 들어갔을때 모든 메시지
   @SubscribeMessage('findAllMessage')
   findAll() {
     return this.messageService.findAll();
   }
 
-  @SubscribeMessage('findOneMessage')
-  findOne(@MessageBody() id: number) {
-    return this.messageService.findOne(id);
+  // 방에 Join
+  @SubscribeMessage('join')
+  joinRoom(
+    @MessageBody('name') name: string,
+    @ConnectedSocket() client: Socket,
+  ) {
+    return this.messageService.identify(name, client.id);
   }
 
-  @SubscribeMessage('updateMessage')
-  update(@MessageBody() updateMessageDto: UpdateMessageDto) {
-    return this.messageService.update(updateMessageDto.id, updateMessageDto);
+  // 타이핑중일때
+  @SubscribeMessage('typing')
+  async typing(
+    @MessageBody('isTyping') isTyping: boolean,
+    @ConnectedSocket() client: Socket,
+  ) {
+    const name = await this.messageService.getClientName(client.id);
+
+    client.broadcast.emit('typing', { name: isTyping });
   }
 
-  @SubscribeMessage('removeMessage')
-  remove(@MessageBody() id: number) {
-    return this.messageService.remove(id);
-  }
+  // @SubscribeMessage('findOneMessage')
+  // findOne(@MessageBody() id: number) {
+  //   return this.messageService.findOne(id);
+  // }
+
+  // @SubscribeMessage('updateMessage')
+  // update(@MessageBody() updateMessageDto: UpdateMessageDto) {
+  //   return this.messageService.update(updateMessageDto.id, updateMessageDto);
+  // }
+
+  // @SubscribeMessage('removeMessage')
+  // remove(@MessageBody() id: number) {
+  //   return this.messageService.remove(id);
+  // }
 }
